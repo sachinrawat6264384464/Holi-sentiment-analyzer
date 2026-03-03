3b967119e218425bf94345c2547f71a1f3326f7a
website <sacinrawat6264384464@gmail.com>
2026-03-04 00:41:14 +0530

    asd

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def predict():
                            prediction_text=sentiment,
                            bg_color=color)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port)
